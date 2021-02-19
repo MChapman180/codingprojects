4358a77c15dc3a04b0478bf7f53809058d6be900
Michael Chapman <mike.chapman84@gmail.com>
2021-02-19 10:54:45 +0700

    update code with edit and date features

--- todo/app.js
+++ todo/app.js
@@ -1,3 +1,8 @@
+// New features todo:
+// 1. Add edit feature inside a todo list. When an edit button is pressed, one can mess with innerHtml.
+// 2. Whenever a todo item is created, somehow show at which time it is created.It should show "Created at 12/12/2021 at 6:16 am UTC" <example>
+// -X-
+
 let todoDiv = document.querySelector("#todo-div");
 let inputText = document.querySelector("#input-text");
 let addBtn = document.querySelector("#addBtn");
@@ -5,7 +10,8 @@ let modalYes = document.querySelector(".yes");
 
 let counter = {
   todos: 0,
-  buttons: 0,
+  completeButtons: 0,
+  editButtons: 0,
 };
 
 // take the input from the text input box
@@ -19,33 +25,65 @@ addBtn.addEventListener("click", function () {
 //if the add button is pressed, add the text from the input to a list item in the todo div
 function createTodo(text) {
   counter.todos += 1;
-  counter.buttons += 1;
+  counter.completeButtons += 1;
+  counter.editButtons += 1;
   todoDiv.innerHTML += `<div class="input-group mb-3">
   <input type="text" class="form-control" id="todo${counter.todos}" value="${text}" readonly>
   <div class="input-group-append completeBtns">
-  <button type="button" class="btn btn-success completeBtn" data-toggle="modal" data-target="#completeModal" id="btn${counter.buttons}">Complete</button>
-</div>
+  <button type="button" class="btn btn-success completeBtn" data-toggle="modal" data-target="#completeModal" id="btn${counter.completeButtons}">Complete</button>
+  </div>
+  <div class="input-group-append editBtns">
+  <button type="button" class="btn btn-info" id="edit${counter.editButtons}">edit</button>
+  </div>
 </div>
 </div>`;
 }
 
-const todoContainer = document.querySelector("#todo-div");
 
+const todoContainer = document.querySelector("#todo-div");
+// if user clicks on the todo container, check if a button is clicked, and then add the modal if click is on complete, or edit otherwise.
 todoContainer.onclick = function (event) {
     event.preventDefault();
     const element = event.target;
     if (element.nodeName === 'BUTTON') {
-      modalYes.addEventListener('click', () => {
-        document.getElementById(event.target.id).classList.remove('btn-success');
-        document.getElementById(event.target.id).classList.add('btn-secondary');
-        element.innerHTML = 'Completed';
-        document.getElementById(event.target.id).disabled = true;
-        let todoTextBox = document.getElementById(`todo${String(element.id.match(/\d/g) || [])}`);
-        console.log(todoTextBox.id);
-        document.getElementById(todoTextBox.id).style.backgroundColor = "#ffd7d7";
-    })   
+      if(element.id.includes('btn')) {
+        addModal(element, event)
+      } else {
+        if(element.id.includes('edit')) {
+          document.getElementById(`btn${String(element.id.match(/\d/g) || [])}`).disabled = false;
+          document.getElementById(`btn${String(element.id.match(/\d/g) || [])}`).classList.remove('btn-secondary');
+          document.getElementById(`btn${String(element.id.match(/\d/g) || [])}`).classList.add('btn-success');
+          document.getElementById(`btn${String(element.id.match(/\d/g) || [])}`).innerHTML = 'Complete';
+        editTodo(element, event);
+        } 
+    } 
     }
 }
+
+// add the colour to the todo list when the complete button is pressed
+function addModal(element, event) {
+  
+  modalYes.addEventListener('click', () => {
+    document.getElementById(event.target.id).classList.remove('btn-success');
+    document.getElementById(event.target.id).classList.add('btn-secondary');
+    element.innerHTML = `Completed at ${new Date()}`;
+    document.getElementById(event.target.id).disabled = true;
+    let todoTextBox = retrieveTodo(element);
+    document.getElementById(todoTextBox.id).style.backgroundColor = "#ffd7d7";
+    document.getElementById(todoTextBox.id).setAttribute('readonly', true);
+})
+}
+// edit the todo when the edit button is pressed and change the innerHTML
+function editTodo(element, event) {
+  let todoText = retrieveTodo(element);
+  document.getElementById(todoText.id).style.backgroundColor = "white";
+  document.getElementById(todoText.id).setAttribute('readonly', false);
+  }
+
+function retrieveTodo(ele) {
+  return document.getElementById(`todo${String(ele.id.match(/\d/g) || [])}`);
+}
+
 
 //attempt 2
 // // I couldn't work out how to use Bootstrap to 'strikethrough' the text when clicking the checkbox.
